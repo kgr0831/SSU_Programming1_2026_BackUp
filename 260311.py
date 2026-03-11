2202c459f5a1da737fdbb1bcd1da4406150bca0d
김가람 <kimgalam0831@gmail.com>
2026-03-11 18:20:01 +0900

    Day_1

--- 260311.py
+++ 260311.py
@@ -1,8 +1,6 @@
 # # 프로그래밍 기초 1 - 2026/03/11(수)
 
-
-
-# # region 기본 도형
+# region turtle 라이브러리
 
 # import turtle
 
@@ -19,22 +17,46 @@
 # #     t.forward(100)
 # #     t.right(90)
 
+# # # #  원
+# import turtle
+
+# t = turtle.Turtle()
+
+# t.shape("turtle")
+# t.circle(50)
+# t.fd(30)
+# t.circle(50)
+
 
 # # turtle.mainloop()
 # # turtle.bye()
 
 # # endregion
 
-# ======================== #
+# ====================================== #
 
+# region 원의 면적 계산
 
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-print("반지름을 입력해주세요 : ")
-radius = int(input())
+# print("반지름을 입력해주세요 : ")
+# radius = int(input())
 
-pi = 3.14
-area = (radius**2) * pi
+# pi = 3.14
+# area = (radius**2) * pi
 
-print(f"반지름 : {radius} / 원의 면적 : {area}")
+# print(f"반지름 : {radius} / 원의 면적 : {area}")
+
+# endregion
+
+# ====================================== #
+
+# region 별까지의 거리 계산하기
+
+distance = 40 * (10**12)
+yearVelocity = 300000 * 3600 * 24 * 365 # 빛이 1년간 가는 거리(광년 속도)
+
+print(distance / yearVelocity)
+
+# endregion
